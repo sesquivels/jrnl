6ac9487b28a7df16ff8dc5c59921f42faab254e8
sesquivels <sesquivels@live.com>
2021-01-31 17:51:23 -0600

    new form of menu with validation

--- jrnl.py
+++ jrnl.py
@@ -18,3 +18,19 @@ print("2)..........................................Exit")
 print("")
 print("")
 menuOption = input("Please enter something, default option will be Exit ")
+if menuOption == "":
+    menuOption = 3
+
+if menuOption== 1:
+    print("local")
+if menuOption==2:
+    print("network")
+
+if menuOption == 3:
+    os.system("clear")
+    sys.exit(0)
+
+
+
+
+
