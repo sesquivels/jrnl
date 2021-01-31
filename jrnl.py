a30cbbd6ab91e0afa6398d125d77afa45d245db7
sesquivels <sesquivels@live.com>
2021-01-31 14:36:37 -0600

    new changes

--- jrnl.py
+++ jrnl.py
@@ -1,10 +1,14 @@
-from simple_term_menu import TerminalMenu
-
-
-def main():
-    terminal_menu = TerminalMenu(["entry 1", "entry 2", "entry 3"])
-    menu_entry_index = terminal_menu.show()
-
-
-if __name__ == "__main__":
-    main()
+print("")
+print("")
+print("Wellcome to JRNL scripting tool")
+print("")
+print("")
+print("************************************************")
+print("******************MENU**************************")
+print("************************************************")
+print("")
+print("")
+print("Local Jornal")
+print("Network Jornal")
+print("")
+print("")
