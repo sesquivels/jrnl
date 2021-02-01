7720f40d354e99b3a4fe112bed68de74b4d2b7a0
sesquivels <sesquivels@live.com>
2021-01-31 19:22:48 -0600

    new form of menu with validation and project properties

--- jrnl.py
+++ jrnl.py
@@ -1,34 +1,67 @@
-import os, sys
+#!/usr/bin/python3
+import os, subprocess, datetime,sys
 
-print("")
-print("")
-print("Wellcome to JRNL scripting tool")
-print("")
-print("")
-print("************************************************")
-print("******************MENU**************************")
-print("************************************************")
-print("")
-print("")
-print("OPTIONS:")
-print("")
-print("1)..................................Local Jornal")
-print("2)................................Network Jornal")
-print("2)..........................................Exit")
-print("")
-print("")
-menuOption = input("Please enter something, default option will be Exit ")
-if menuOption == "":
-    menuOption = 3
+class osInteract:
+    #atributes
+    version = ""
+    hostname = ""
+    osdate = datetime.date
+    menuOption = ""
+    environmentOption = ""
 
-if menuOption== 1:
-    print("local")
-if menuOption==2:
-    print("network")
 
-if menuOption == 3:
-    os.system("clear")
-    sys.exit(0)
+    #methods
+    #initialized class
+    def __init__(self):
+      return
+
+    def __del__(self):
+      return
+
+    def timeOS(self):
+        self.osdate = os.popen('date ').read()
+        print(self.osdate)
+
+    def menu(self):
+        os.system("clear")
+        print("")
+        print("")
+        print("Wellcome to JRNL scripting tool")
+        print("")
+        print("")
+        print("************************************************")
+        print("******************MENU**************************")
+        print("************************************************")
+        print("")
+        print("")
+        print("OPTIONS:")
+        print("")
+        print("1)..................................Local Jornal")
+        print("2)................................Network Jornal")
+        print("2)..........................................Exit")
+        print("")
+        print("")
+        menuOption = input("Please enter something, default option will be Exit ")
+        if self.menuOption == "":
+            self.osdatemenuOption = 3
+            print("pressed Enter will closed")
+        if self.menuOption == "1":
+            print("local")
+        if self.menuOption == "2":
+            print("network")
+
+        if self.menuOption == 3:
+            os.system("clear")
+            sys.exit(0)
+
+    def evironment(self):
+        print("Wellcome to jrnl app")
+        self.environmentOption = input("please set which pc is this, laptop, pi, workstation or  nas: ")
+        print("you choose:nas " + self.environmentOption)
+
+
+m1 = osInteract()
+m1.evironment()
 
 
 
